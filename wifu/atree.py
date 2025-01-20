09ec9d77a2029bc69a2746ca3ee8e8afd6f3a48a
Krai53n <greasha46@gmail.com>
2025-01-20 23:45:17 +0300

    ok

--- wifu/atree.py
+++ wifu/atree.py
@@ -3,52 +3,68 @@ from astree import AST, FuncCall, FuncCallArg
 
 class Atom:
     def __init__(self, data: str):
-        pass
+        self.data = data
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}[{self.data}]'
 
 
 class Str(Atom):
-    def __init__(self, data: str):
-        self.data = data
+    pass
 
 
 class Char(Atom):
-    def __init__(self, data: str):
-        self.data = data
+    pass
 
 
 class Int(Atom):
-    def __init__(self, data: int):
-        self.data = data
+    pass
 
 
 class Float(Atom):
-    def __init__(self, data: float):
-        self.data = data
+    pass
+
+
+class Fraction(Atom):
+    pass
 
 
 def get_atom(data: str) -> Atom:
     fst = data[0]
-    if fst in ("'", '"'):
-        if len(data) == 1 and fst == "'":
+    if fst in ("'", '"') and len(data) >= 2:
+        if len(data) == 3 and fst == "'":
             return get_as_char_atom(data)
         return get_as_str_atom(data)
-    if fst.isdigit() or fst == '.':
-        return get_as_num(data)
-
-
-def get_as_num(data) -> Atom:
-    pass
+    if (fst.isdigit() or
+            fst == '.' or
+            (fst == '-' and )):
+        return get_as_num_atom(data)
 
 
 def get_as_char_atom(data: str) -> Atom:
-    pass
+    return Char(data[1])
 
 
 def get_as_str_atom(data: str) -> Atom:
-    pass
+    for i, c in enumerate(data[1:]):
+        if c == data[0]:
+            # print(Str(data[1:i+1]))
+            return Str(data[1:i+1])
+    raise SyntaxError('string literal should be closed with \' or " in the same line')
 
 
 def get_as_num_atom(data: str) -> Atom:
+    # - float
+    #   - signed
+    #   - unsigned
+    # - int
+    #   - signed
+    #   - unsigned
+    # - fraction
+    #   - signed
+    #   - unsigned
+    is_signed = False
+    is_mantissa = False
     pass
 
 
